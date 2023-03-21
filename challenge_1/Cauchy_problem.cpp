44fa40f1d69873d309e0dcbdc574f84b7dcc9f52
pietr <pietro1.devecchi@mail.polimi.it>
2023-03-21 09:41:20 +0000

    sk

---
+++ challenge_1/Cauchy_problem.cpp
@@ -0,0 +1,47 @@
+#include "Cauchy_problem.hpp"
+#include "basicZeroFun.hpp"
+
+#include <algorithm>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+// to compile:
+// g++ Cauchy_problem.cpp -o  Cauchy_problem -I/home/pietr/pacs_directory/pacs-examples/Examples/src/LinearAlgebraUtil -I/home/pietr/pacs_directory/pacs-examples/Examples/include -I/home/pietr/pacs_directory/pacs-examples/Examples/src/Utilities
+
+
+
+void mysolver(force_type f, double y_0, double T, unsigned N, output & osk){
+    double h=T/N;
+    for(size_t i=0; i<=N; ++i){
+        osk.t_n.push_back(0+T/N*i);
+    }
+    auto F = [=](double x,double t_1,double t_2,double u_n){
+        return x-h/2*(f(t_1, x)+f(t_2, u_n))-u_n;
+    };
+    osk.u_n.push_back(y_0);
+
+    for(size_t i=1; i<=N; ++i){
+        auto F_x = [=](double x){
+            return F(x, osk.t_n[i-1], osk.t_n[i], osk.u_n[i-1]);
+        }; 
+        std::tuple<double, bool> gio = apsc::secant(F_x, -10000., +10000.);
+        osk.u_n.push_back(std::get<0> (gio));
+    }
+};
+
+// constexpr int func( int , int ){return 1;};
+
+// int main(){
+
+//  // a function
+// struct F2{ // a function object
+// int operator ()( int , int ) const {return 0;} ;};
+// // a vector of functions
+// std::vector<std::function< int ( int , int )>> tasks ;
+// tasks.push_back(func );// wraps a function
+// tasks.push_back(F2{});// wraps a functor
+// tasks.push_back([]( int x, int y){ return x*y;});// a lambda
+// for ( auto i : tasks) std::cout<<i(3,4)<<std::endl ;
+
